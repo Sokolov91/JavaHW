5b8d8e836b7b62bc27a60e0c69b1fb2f70169f0f
Sokolov <yevhens91@gmail.com>
2020-10-22 18:00:33 +0300

    my first commit

---
+++ src/main/java/task_1/Main.java
@@ -0,0 +1,17 @@
+package task_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumOfNumbers(25,33.9));
+    }
+
+    public static int sumOfNumbers(int a, int b){
+        return a + b;
+    }
+
+    public static double sumOfNumbers(double a, double b){
+        return a + b;
+    }
+
+}
